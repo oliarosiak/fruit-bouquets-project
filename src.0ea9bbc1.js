5bdf5b946b84adb90448277fc1909c167ab3e4de
Oleksandra9 <89663143+Oleksandra9@users.noreply.github.com>
2021-11-29 15:36:03 +0000

    Deploying to gh-pages from @ oliarosiak/fruit-bouquets-project@e6c06162d3f8645ee7d36e0673b3de6c5d42480b 🚀

--- src.0ea9bbc1.js
+++ src.0ea9bbc1.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/fruit-bouquets-project/src.6e0b580f.js.map
+//# sourceMappingURL=/fruit-bouquets-project/src.0ea9bbc1.js.map
